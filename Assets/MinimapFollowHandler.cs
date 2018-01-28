5f452ff495bb2a7d8067ba2675b0d726b2e51650
Fergui <fergui.pascual@rubiconpublishing.com>
2018-01-28 08:41:33 -0500

    Set minimap to renderTexture (canvas); Add a backdrop/border to the minimap; Set minimap camera to follow player;

---
+++ Assets/MinimapFollowHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollowHandler : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 newPosition = target.position;
+		newPosition.z = transform.position.z;
+
+		transform.position = newPosition;
+	}
+}
